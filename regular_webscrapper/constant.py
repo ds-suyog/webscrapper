599214f32cd099fb562de1f847bb8b910559f535
ds-suyog <suyog.sethia@gmail.com>
2019-05-15 03:24:21 +0530

    updated files

--- regular_webscrapper/constant.py
+++ regular_webscrapper/constant.py
@@ -1,1 +1,3 @@
-URL = "https://www.bseindia.com/"
+BASE_URL = "https://www.bseindia.com/"
+URL_GAINER = "https://api.bseindia.com/BseIndiaAPI/api/MktRGainerLoserData/w?GLtype=gainer&IndxGrp=AllMkt&IndxGrpval=AllMkt&orderby=all"
+URL_LOOSER = "https://api.bseindia.com/BseIndiaAPI/api/MktRGainerLoserData/w?GLtype=loser&IndxGrp=AllMkt&IndxGrpval=AllMkt&orderby=all"
