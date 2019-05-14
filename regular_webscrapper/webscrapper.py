b6b256719be11d284ba5c964d23469a35e59a1e8
ds-suyog <suyog.sethia@gmail.com>
2019-05-15 02:52:02 +0530

    added webscrapper

--- regular_webscrapper/webscrapper.py
+++ regular_webscrapper/webscrapper.py
@@ -1,44 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+#import constant
+import json
 
 class Webscrapper:
-	_url = "https://www.bseindia.com/"
 
 	def __init__(self):
-		pass
+		self._url = "https://www.bseindia.com/"
 
 	def crawl(self):
-		page = requests.get(Webscrapper._url)  #<class 'requests.models.Response'>
+		page = requests.get(self._url)  
+		#constant.URL		
 		print(page.headers['content-type'])
 		print(page.encoding)
-		print(page.status_code)  # 200 => success, 403 => forbidden, 404 => not found
+		print(page.status_code)  
 
-		params = {'filter': ''} 
-		#c = page.content   # <class 'bytes'>
-		txt = page.text	# <class 'str'>
+		request_url = "https://api.bseindia.com/BseIndiaAPI/api/MktRGainerLoserData/w?GLtype=gainer&IndxGrp=AllMkt&IndxGrpval=AllMkt&orderby=all"
+		params = {'GLtype':	'gainer', 'IndxGrp':	'AllMkt', 'IndxGrpval':	'AllMkt', 'orderby':	'all'}
+		page = requests.get(url = request_url, params = params,) 
+		print(page.status_code)
+		page.raise_for_status()
+		page_txt = page.text	
 
-		# with open("content.txt", 'r+') as f:
-		# 	print(f.write(txt))	
+		with open("content.html", 'r+') as f:
+			print(f.write(page_txt))	
+
 		return page
 
-	def parse(self, page):
-		soup = BeautifulSoup(page.text, features="html.parser") #  <class 'bs4.BeautifulSoup'>, by default it uses features as best available html-parser
-		for link in soup.find_all('a'):
-			print(link.get('href'))
 
-		# with open("content.txt", 'r+') as f:
-		# 	f.truncate()
+	def parse(self, page):
+		print("parsing...")
+
+		with open("content.html", encoding="utf8") as f:
+			contents = f.read()
+		soup = BeautifulSoup(contents, "html.parser")
+		
+		#soup = BeautifulSoup(page.text, features="html.parser") #  <class 'bs4.BeautifulSoup'>, by default it uses features as best available html-parser
+		
+		data = json.loads(soup.text)
+		print(type(data))		
+		print(data.keys())
+
+
 
 
 def main():
 	ws = Webscrapper()	
-	page = ws.crawl()
-	ws.parse(page)
+	#page = ws.crawl()
+	ws.parse(page = 'no need to pass for now')
 
 if __name__ == '__main__':
 	main()
-
-		# with open("content.txt", 'r+') as f:
-		# 	print(f.write(txt))	
-
