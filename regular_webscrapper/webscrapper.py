86461b4e8d5c8d86c1a7398341dd53ac3f4e6392
ds-suyog <suyog.sethia@gmail.com>
2019-05-15 03:24:37 +0530

    added code

--- regular_webscrapper/webscrapper.py
+++ regular_webscrapper/webscrapper.py
@@ -1,54 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-#import constant
+import constant
 import json
 
 class Webscrapper:
 
 	def __init__(self):
-		self._url = "https://www.bseindia.com/"
+		pass
 
 	def crawl(self):
-		page = requests.get(self._url)  
-		#constant.URL		
-		print(page.headers['content-type'])
-		print(page.encoding)
-		print(page.status_code)  
 
-		request_url = "https://api.bseindia.com/BseIndiaAPI/api/MktRGainerLoserData/w?GLtype=gainer&IndxGrp=AllMkt&IndxGrpval=AllMkt&orderby=all"
-		params = {'GLtype':	'gainer', 'IndxGrp':	'AllMkt', 'IndxGrpval':	'AllMkt', 'orderby':	'all'}
-		page = requests.get(url = request_url, params = params,) 
-		print(page.status_code)
-		page.raise_for_status()
-		page_txt = page.text	
+		params_g = {'GLtype':	'gainer', 'IndxGrp':	'AllMkt', 'IndxGrpval':	'AllMkt', 'orderby':	'all'}
+		page_g = requests.get(url = constant.URL_GAINER, params = params_g,) 
+		print(page_g.status_code)
+		page_g.raise_for_status()	
+		#Note: next pages are stored as well, which 
+		with open("content_gainer.html", 'r+') as f:
+			print(f.write(page_g.text))	
 
-		with open("content.html", 'r+') as f:
-			print(f.write(page_txt))	
+		params_l = {'GLtype':	'loser', 'IndxGrp':	'AllMkt', 'IndxGrpval':	'AllMkt', 'orderby':	'all'}
+		page_l = requests.get(url = constant.URL_LOOSER, params = params_l,) 
+		print(page_l.status_code)
+		page_l.raise_for_status()	
+		with open("content_looser.html", 'r+') as f:
+			print(f.write(page_l.text))	
 
-		return page
 
 
 	def parse(self, page):
 		print("parsing...")
 
-		with open("content.html", encoding="utf8") as f:
+		with open("content_gainer.html", encoding="utf8") as f:
 			contents = f.read()
 		soup = BeautifulSoup(contents, "html.parser")
-		
-		#soup = BeautifulSoup(page.text, features="html.parser") #  <class 'bs4.BeautifulSoup'>, by default it uses features as best available html-parser
-		
+
 		data = json.loads(soup.text)
 		print(type(data))		
 		print(data.keys())
 
 
 
-
 def main():
 	ws = Webscrapper()	
-	#page = ws.crawl()
-	ws.parse(page = 'no need to pass for now')
+	page = ws.crawl()
+	#ws.parse(page = 'no need to pass for now')
 
 if __name__ == '__main__':
 	main()
