4f9d3e33723165c988ed720aa4d6f6acfcf69105
ds-suyog <suyog.sethia@gmail.com>
2019-05-19 02:40:40 +0530

    added simple mailer

--- regular_webscrapper/libs/mailer.py
+++ regular_webscrapper/libs/mailer.py
@@ -1,20 +1,14 @@
 import smtplib  
+import git, sys; sys.path.append("{}/regular_webscrapper".format(git.Repo('.', search_parent_directories=True).working_tree_dir))
+import constant
 
-def send_mail(fromaddr="myworker11@gmail.com", password="worker@MAILER#11", subject = "", msg = "request received, processing request...", toaddr="ssethia86@gmail.com"):
-	#creates SMTP session 
+def send_mail(fromaddr = constant.WORKER_EMAIL, password = constant.WORKER_PASS, subject = '', msg, toaddr):
 	s = smtplib.SMTP('smtp.gmail.com', 587) 
-  
-	# start TLS for security 
 	s.starttls() 
-  
-	# Authentication 
 	s.login(fromaddr, password) 
-  
-	message = msg
-	message = 'Subject: {}\n\n{}'.format(subject, message)
-	# for multiple receivers: replace with recvr_list, where recvr_list = ["xx@gmail.com", "yy@gmail.com"] 
+	message = 'Subject: {}\n\n{}'.format(subject, msg)
 	s.sendmail(fromaddr, toaddr, message)
-	# s.quit() 
+	s.quit() 
 
 def main():
 	send_mail()
