bda2a8ffce0f3eb2374be0dc88d2a9144212b230
ds-suyog <suyog.sethia@gmail.com>
2019-05-26 00:03:22 +0530

    adding spider

--- scrapy_webscrapper/webscrap_bot_project/webscrap_bot_project/spiders/bse_spider.py
+++ scrapy_webscrapper/webscrap_bot_project/webscrap_bot_project/spiders/bse_spider.py
@@ -1,1 +1,11 @@
 import scrapy
+
+class ExtractUrls(scrapy.Spider): 
+    name = "extract"                 
+  
+    def start_requests(self): 
+        urls = ['https://www.bseindia.com/', ] 
+          
+        for url in urls: 
+            yield scrapy.Request(url = url, callback = self.parse) 
+
