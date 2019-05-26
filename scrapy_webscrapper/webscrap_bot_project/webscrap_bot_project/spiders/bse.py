02951d876a2b964bbcbcae2aa44af467527cadf7
ds-suyog <suyog.sethia@gmail.com>
2019-05-26 19:03:34 +0530

    rstructured spiders

--- scrapy_webscrapper/webscrap_bot_project/webscrap_bot_project/spiders/bse.py
+++ scrapy_webscrapper/webscrap_bot_project/webscrap_bot_project/spiders/bse.py
@@ -1,13 +1,30 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
-class BseSpider(scrapy.Spider):
-	name = "bse"
-	print("==============")
+class GainersSpider(scrapy.Spider):
+	name = "bse_gainers"
 	def start_requests(self):
-		url = 'https://www.bseindia.com/'
+		url = "https://www.bseindia.com/"
 		yield scrapy.Request(url = url, callback = self.parse)
 
 	def parse(self, response):
-		print("========     =============       ===========")
+		print("======   ========== bse_gainers: parse!!")
+
+class LoosersSpider(scrapy.Spider):
+	name = "bse_loosers"
+	def start_requests(self):
+		urls = "https://api.bseindia.com/BseIndiaAPI/api/MktRGainerLoserData/w?GLtype=gainer&IndxGrp=AllMkt&IndxGrpval=AllMkt&orderby=all"
+		yield scrapy.Request(url = url, callback = self.parse)
+
+	def parse(self, response):
+		print("======   ========== bse_loosers: parse!!")
+
+class TrendingSpider(scrapy.Spider):
+	name = "bse_trending"
+	def start_requests(self):
+		url = "https://api.bseindia.com/BseIndiaAPI/api/MktRGainerLoserData/w?GLtype=loser&IndxGrp=AllMkt&IndxGrpval=AllMkt&orderby=all"
+		yield scrapy.Request(url = url, callback = self.parse)
+
+	def parse(self, response):
+		print("======   ========== bse_trending: parse!!")
 
