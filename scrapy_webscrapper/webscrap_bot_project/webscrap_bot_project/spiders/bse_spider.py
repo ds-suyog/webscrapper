225e1d772ce71a0e6e0dfc8f5491bb26b5dd1e64
ds-suyog <suyog.sethia@gmail.com>
2019-05-26 17:26:06 +0530

    spider

--- scrapy_webscrapper/webscrap_bot_project/webscrap_bot_project/spiders/bse_spider.py
+++ scrapy_webscrapper/webscrap_bot_project/webscrap_bot_project/spiders/bse_spider.py
@@ -1,11 +1,20 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
 
-class ExtractUrls(scrapy.Spider): 
+class BseSpider(scrapy.Spider): 
     name = "extract"                 
-  
     def start_requests(self): 
-        urls = ['https://www.bseindia.com/', ] 
-          
+        urls = ['https://www.bseindia.com/', ]        
         for url in urls: 
             yield scrapy.Request(url = url, callback = self.parse) 
 
+        def parse(self, response):
+        	title = response.css('title::text').extract_first()
+        	links = response.css('a::attr(href)').extract()
+
+        	for link in links:
+        		yield
+        		{
+        		'title': title,
+        		'links': link
+        		} 
